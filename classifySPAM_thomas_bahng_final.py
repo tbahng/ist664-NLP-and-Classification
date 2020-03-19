da56e22181df92020cf2bcd13a0c8ec8ddc2b66c
Thomas H Bahng <thomas.h.bahng@boeing.com>
2020-03-19 01:34:33 -0700

    finalize report

--- classifySPAM_thomas_bahng_final.py
+++ classifySPAM_thomas_bahng_final.py
@@ -2,6 +2,7 @@
 This program reads email data for the spam classification problem and can be executed from an IDE or command line.
 This program expects the following directory structure for proper execution:
 - project folder
-    - classifySPAM.py
+    - classifySPAM_thomas_bahng_final.py
+    
         
 """
